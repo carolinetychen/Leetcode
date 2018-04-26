eeabcdbf41830e97778a992e81312589ac19e7aa
Caroline Chen <carolinetychen@gmail.com>
2018-04-26 23:33:30 +0900

    explanation added

--- Algorithms/ToeplitzMatrix/ToeplitzMatrix.cpp
+++ Algorithms/ToeplitzMatrix/ToeplitzMatrix.cpp
@@ -1,3 +1,24 @@
+// *************************************************
+// Leetcode Solution
+// Author: Caroline Chen
+// *************************************************
+// A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+// Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+// Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+// Output: True
+// Explanation:
+// 1234
+// 5123
+// 9512
+
+// In the above grid, the diagonals are 
+// "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]",
+// and in each diagonal all elements are the same, so the answer is True.
+// *************************************************
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
