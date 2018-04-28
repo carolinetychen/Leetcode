af582e2dd89020a9298223d4cfe12fb833683ecd
Caroline Chen <carolinetychen@gmail.com>
2018-04-28 21:20:12 +0900

    add explanation

--- Algorithms/InvertBinaryTree/InvertBinaryTree.cpp
+++ Algorithms/InvertBinaryTree/InvertBinaryTree.cpp
@@ -2,6 +2,18 @@
 // Leetcode Solution
 // Author: Caroline Chen
 // *************************************************
+// Invert a binary tree.
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// to 
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
 
 // Time complexity: O(n)
 class Solution {
